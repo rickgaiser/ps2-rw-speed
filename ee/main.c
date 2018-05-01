b1af036eeb77ea1bc3c91d3cc39c25c4deed5c68
Rick Gaiser <rgaiser@gmail.com>
2018-05-01 21:55:27 +0200

    Add screen debugging

--- ee/main.c
+++ ee/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <kernel.h>
+#include <debug.h>
 #include <sifrpc.h>
 #include <fileXio_rpc.h>
 #include <loadfile.h>
@@ -10,6 +11,9 @@
 
 
 #define CLOCKS_PER_MSEC (147456 / 256)
+
+//#define PRINTF printf
+#define PRINTF scr_printf
 
 
 //--------------------------------------------------------------
@@ -34,7 +38,7 @@ void read_test(const char * filename, unsigned int buf_size, unsigned int max_si
     clock_t clk_start, clk_end;
 
 	if ((fp = fopen(filename, "rb")) == NULL) {
-		printf("Could not find '%s'\n", filename);
+		PRINTF("Could not find '%s'\n", filename);
 		return;
 	}
 
@@ -50,7 +54,7 @@ void read_test(const char * filename, unsigned int buf_size, unsigned int max_si
 	while (size_left > 0) {
         int read_size = (size_left > buf_size) ? buf_size : size_left;
         if (fread(buffer, 1, read_size, fp) != read_size) {
-            printf("Failed to read file.\n");
+            PRINTF("Failed to read file.\n");
             return;
         }
         size_left -= read_size;
@@ -58,7 +62,7 @@ void read_test(const char * filename, unsigned int buf_size, unsigned int max_si
     clk_end = clock();
 
 	msec = (int)((clk_end - clk_start) / CLOCKS_PER_MSEC);
-	printf("Read %dKiB in %dms, buf_size=%d, speed=%dKB/s\n", fd_size/1024, msec, buf_size, fd_size / msec);
+	PRINTF("Read %dKiB in %dms, buf_size=%d, speed=%dKB/s\n", fd_size/1024, msec, buf_size, fd_size / msec);
 
 	free(buffer);
 
@@ -71,23 +75,23 @@ void test_cdvd()
 	sceCdRMode mode = {1, 0, 0, 0};
     char * buffer = NULL;
 
-	printf("Start reading CDVD\n");
+	PRINTF("Start reading CDVD\n");
 
 	if (sceCdInit(SCECdINIT) != 1)
-		printf("ERROR: sceCdInit(SCECdINIT)\n");
+		PRINTF("ERROR: sceCdInit(SCECdINIT)\n");
 
 	buffer = malloc(512*1024);
 
 	if (sceCdReadDVDV(0, 1, buffer, &mode) != 1)
-		printf("ERROR: sceCdReadDVDV\n");
+		PRINTF("ERROR: sceCdReadDVDV\n");
 
 	if (sceCdSync(0) != 0)
-		printf("ERROR: sceCdSync\n");
+		PRINTF("ERROR: sceCdSync\n");
 
 	free(buffer);
 
 	//if (sceCdInit(SCECdEXIT) != 1)
-	//	printf("ERROR: sceCdInit(SCECdEXIT)\n");
+	//	PRINTF("ERROR: sceCdInit(SCECdEXIT)\n");
 }
 
 //--------------------------------------------------------------
@@ -97,8 +101,8 @@ int main()
     unsigned int buf_size;
 #endif
 
-    printf("EE Read/Write speed test\n");
-    printf("------------------------\n");
+    PRINTF("EE Read/Write speed test\n");
+    PRINTF("------------------------\n");
 
 	SifInitRpc(0);
 
@@ -106,60 +110,69 @@ int main()
 	 * Load IO modules
 	 */
 	if (SifLoadModule("host:modules/iomanX.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/iomanX.irx'\n");
+		PRINTF("Could not load 'host:modules/iomanX.irx'\n");
 
 	if (SifLoadModule("host:modules/fileXio.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/fileXio.irx'\n");
+		PRINTF("Could not load 'host:modules/fileXio.irx'\n");
 
 	if (SifLoadModule("host:modules/usbd.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/usbd.irx'\n");
+		PRINTF("Could not load 'host:modules/usbd.irx'\n");
 
 	if (SifLoadModule("host:modules/iLinkman.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/iLinkman.irx'\n");
+		PRINTF("Could not load 'host:modules/iLinkman.irx'\n");
 
 #ifdef TEST_USB
 #ifdef USE_BDM
 	if (SifLoadModule("host:modules/bdm.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/bdm.irx'\n");
+		PRINTF("Could not load 'host:modules/bdm.irx'\n");
 
 	if (SifLoadModule("host:modules/usbmass_bd.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/usbmass_bd.irx'\n");
+		PRINTF("Could not load 'host:modules/usbmass_bd.irx'\n");
 
 	if (SifLoadModule("host:modules/bdmfs_vfat.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/bdmfs_vfat.irx'\n");
+		PRINTF("Could not load 'host:modules/bdmfs_vfat.irx'\n");
+
+	if (SifLoadModule("host:modules/bdmfs_ext2.irx", 0, NULL) < 0)
+		PRINTF("Could not load 'host:modules/bdmfs_ext2.irx'\n");
 #else
 	if (SifLoadModule("host:modules/usbhdfsd.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/usbhdfsd.irx'\n");
+		PRINTF("Could not load 'host:modules/usbhdfsd.irx'\n");
 #endif
 #endif
 
 #ifdef TEST_MC2SD
 	if (SifLoadModule("host:modules/bdm.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/bdm.irx'\n");
+		PRINTF("Could not load 'host:modules/bdm.irx'\n");
 
 	//if (SifLoadModule("host:modules/sio2man.irx", 0, NULL) < 0)
-	//	printf("Could not load 'host:modules/sio2man.irx'\n");
+	//	PRINTF("Could not load 'host:modules/sio2man.irx'\n");
 
 	if (SifLoadModule("host:modules/mc2sd_bd.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/mc2sd_bd.irx'\n");
+		PRINTF("Could not load 'host:modules/mc2sd_bd.irx'\n");
 
 	if (SifLoadModule("host:modules/bdmfs_vfat.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/bdmfs_vfat.irx'\n");
+		PRINTF("Could not load 'host:modules/bdmfs_vfat.irx'\n");
+
+	if (SifLoadModule("host:modules/bdmfs_ext2.irx", 0, NULL) < 0)
+		PRINTF("Could not load 'host:modules/bdmfs_ext2.irx'\n");
 #endif
 
 #ifdef TEST_IEEE
 #ifdef USE_BDM
 	if (SifLoadModule("host:modules/bdm.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/bdm.irx'\n");
+		PRINTF("Could not load 'host:modules/bdm.irx'\n");
 
 	if (SifLoadModule("host:modules/IEEE1394_bd.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/IEEE1394_bd.irx'\n");
+		PRINTF("Could not load 'host:modules/IEEE1394_bd.irx'\n");
 
 	if (SifLoadModule("host:modules/bdmfs_vfat.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/bdmfs_vfat.irx'\n");
+		PRINTF("Could not load 'host:modules/bdmfs_vfat.irx'\n");
+
+	if (SifLoadModule("host:modules/bdmfs_ext2.irx", 0, NULL) < 0)
+		PRINTF("Could not load 'host:modules/bdmfs_ext2.irx'\n");
 #else
 	if (SifLoadModule("host:modules/IEEE1394_disk.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/IEEE1394_disk.irx'\n");
+		PRINTF("Could not load 'host:modules/IEEE1394_disk.irx'\n");
 #endif
 #endif
 
@@ -171,34 +184,34 @@ int main()
 	 * Load HDD modules
 	 */
 	//if (SifLoadModule("host:modules/ps2dev9.irx", 0, NULL) < 0)
-	//	printf("Could not load 'host:modules/ps2dev9.irx'\n");
+	//	PRINTF("Could not load 'host:modules/ps2dev9.irx'\n");
 
 	if (SifLoadModule("host:modules/ps2atad.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/ps2atad.irx'\n");
+		PRINTF("Could not load 'host:modules/ps2atad.irx'\n");
 
 	if (SifLoadModule("host:modules/ps2hdd.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/ps2hdd.irx'\n");
+		PRINTF("Could not load 'host:modules/ps2hdd.irx'\n");
 
 	if (SifLoadModule("host:modules/ps2fs.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/ps2fs.irx'\n");
+		PRINTF("Could not load 'host:modules/ps2fs.irx'\n");
 
 	fileXioInit();
 	if (fileXioMount("pfs0:", "hdd0:__system", FIO_MT_RDWR) == -ENOENT)
-            printf("Could not mount 'hdd0:__system'\n");
+            PRINTF("Could not mount 'hdd0:__system'\n");
 #endif
 
 #ifdef TEST_USB_CDVD
 	if (SifLoadModule("host:cdvdman/cdvdman.irx", 0, NULL) < 0)
-		printf("Could not load 'host:cdvdman/cdvdman.irx'\n");
+		PRINTF("Could not load 'host:cdvdman/cdvdman.irx'\n");
 
 	if (SifLoadModule("host:cdvdfsv/cdvdfsv.irx", 0, NULL) < 0)
-		printf("Could not load 'host:cdvdfsv/cdvdfsv.irx'\n");
+		PRINTF("Could not load 'host:cdvdfsv/cdvdfsv.irx'\n");
 
 	if (SifLoadModule("host:modules/usbmass_bd.irx", 0, NULL) < 0)
-		printf("Could not load 'host:modules/usbmass_bd.irx'\n");
+		PRINTF("Could not load 'host:modules/usbmass_bd.irx'\n");
 
 	//if (SifLoadModule("host:modules/IEEE1394_bd.irx", 0, NULL) < 0)
-	//	printf("Could not load 'host:modules/IEEE1394_bd.irx'\n");
+	//	PRINTF("Could not load 'host:modules/IEEE1394_bd.irx'\n");
 #endif
 
 
@@ -226,10 +239,10 @@ int main()
 	 * Start read/write speed test on the IOP
 	 */
 	if (SifLoadModule("host:iop/rw_speed.irx", 0, NULL) < 0)
-		printf("Could not load 'host:iop/rw_speed.irx'\n");
+		PRINTF("Could not load 'host:iop/rw_speed.irx'\n");
 #endif
 
-	printf("Done, exit in 5\n");
+	PRINTF("Done, exit in 5\n");
 	delay(5);
 
 	return 0;
